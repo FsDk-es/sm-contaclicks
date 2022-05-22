e0a1f32170bc0443da4bf9dc074ec96de3ee458c
FsDk <cayuela@gmail.com>
2022-05-22 03:38:42 +0200

    se crean 2 componentes y se agregan. y se hacen trabajar mediante props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
 import freeCodeCampLogo from './logo/fcc_primary_large.svg';
+import Boton from './componentes/Boton.jsx';
+import Contador from './componentes/Contador.jsx';
+
 function App() {
+	const manejarClick = () => {
+		console.log('Click');
+	};
+	const reiniciarContador = () => {
+		console.log('Reiniciar');
+	};
 	return (
 		<div className="App">
 			<div className="freecodecamp-logo-contenedor">
@@ -9,6 +18,19 @@ function App() {
 					src={freeCodeCampLogo}
 					alt="Logo de freeCodeCamp"
 					description="La imagen del logo jeje"
+				/>
+			</div>
+			<div className="contenedor-principal">
+				<Contador />
+				<Boton
+					texto="Click"
+					esBotonDeClick={true}
+					manejarClick={manejarClick}
+				/>
+				<Boton
+					texto="Reiniciar"
+					esBotonDeClick={false}
+					manejarClick={reiniciarContador}
 				/>
 			</div>
 		</div>
---
+++ src/componentes/Contador.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../css/contador.css'
+function Contador({numClicks}) {
+  return (
+    <div className="contador">25</div>
+  );
+}
+
+export default Contador
