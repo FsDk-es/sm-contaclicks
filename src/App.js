5243611bac16610891f58151f95af15fe6000390
FsDk <cayuela@gmail.com>
2022-05-22 02:32:03 +0200

    se agregan algunas caracteristicas basicas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import freeCodeCampLogo from './logo/fcc_primary_large.svg';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="freecodecamp-logo-contenedor">
+				<img
+					className="freecodecamp-logo"
+					src={freeCodeCampLogo}
+					alt="Logo de freeCodeCamp"
+					description="La imagen del logo jeje"
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
